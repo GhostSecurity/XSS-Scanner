e368c257d1bc737778c6c577d257ab50215cdc80
HydraBoy <mosydev2017@gmail.com>
2017-07-06 17:24:09 -0400

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,1 +1,72 @@
 # XSS Scanner
+
+
+def check_for_update():
+    admin_github_url = "https://github.com/HydraBoy/XSS-Scanner"
+    keyword = "ADMIN_FINDER_VERSION = '"
+    updated = False
+    print "\n[*] Checking for XSS-Scanner updates.."
+    time.sleep(1)
+    try:
+        http = urllib2.urlopen('https://raw.githubusercontent.com/HydraBoy/XSS-Scanner/master/.version.txt',data=None)
+        content = http.read()
+        read = open('.version.txt','r').read()
+        if read == content:
+            print '[#] No updates available.'
+        else:
+            print '[+] Updating XSS-Scanner Tool...'
+            os.popen("git pull "+ admin_github_url)
+            print '[+] XSS Scanner Updated To Version: ' + content
+            updated = True
+    except Exception as ex:
+        print "\n[!] Problem while updating."
+    if updated:
+        sys.exit(Fore.GREEN+"[!] Plase Relaunch The Script.")
+       
+      
+# Check If URL in available URL.
+ 
+  
+def IsUrl(url):
+    if 'https://' not in str(url):
+        if 'http://' in str(url):
+            XSSFind(url)
+        else:
+
+            XSSFind("http://" + str(url))
+    else:
+        if 'http://' not in str(url):
+            if 'https://' in str(url):
+                XSSFind(url)
+            else:
+                XSSFind("http://" + str(url))
+                
+                
+# XSS Scanner. Check If Target URL has XSS Bug.
+
+
+def XSSFind(path=None,url):
+  try:
+    urls_path = open(path).readlines()
+    
+    # For Loop For Read Lines
+    
+    for link in urls_path:
+      if '=' not in link:
+        print(Fore.YELLOW+"[%s] [ERROR] URL [%s]" % (datatime.now(), link))
+      else:
+           _GET = urllib2.urlopen(link+'"<b>Ghost Security</b>',data=None)
+           content = http.read()
+           if 'Ghost Security' in content:
+              print(Fore.RED+"[%s] [XSS] URL [%s]" % (datatime.now(), link))
+              raw_input(Fore.GREEN+"[%s] [CONTINUE] [PRESS ENTER TO CONTINUE]" % (datatime.now()))
+           else:
+              print(Fore.GREEN+"[%s] [NEXT] URL [%s]" % (datatime.now(), link))
+              
+    raw_input(Fore.YELLOW+"[%s] [EXIT] [PRESS ENTER TO EXIT]" % (datatime.now()))
+    
+
+
+           
+      
+  
